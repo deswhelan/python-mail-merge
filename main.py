1654c08e93954f2836cc5f4c1a7f29f94c5567d4
Des Whelan <wheland6@tcd.ie>
2025-10-29 12:18:38 +0000

    refactor naming convention, output doc type

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-with open("./Input/Names/invited_names.txt") as invited_names:
-    names = invited_names.read().split("\n")
+NAME_PLACEHOLDER = "[name]"
 
-with open("./Input/Letters/starting_letter.txt") as starting_letter:
-    raw_starting_letter = starting_letter.read()
+with open("./Input/Names/invited_names.txt") as invited_names_file:
+    names = invited_names_file.read().split("\n")
+
+with open("./Input/Letters/starting_letter.txt") as starting_letter_file:
+    letter_template = starting_letter_file.read()
 
     for name in names:
-        personalised_letter_text = raw_starting_letter.replace("[name]", name)
-        with open(f"./Output/ReadyToSend/{name}.txt", "w") as personalised_letter:
+        personalised_letter_text = letter_template.replace(NAME_PLACEHOLDER, name)
+        with open(f"./Output/ReadyToSend/{name} invite.docx", "w") as personalised_letter:
             personalised_letter.write(personalised_letter_text)
